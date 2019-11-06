00960d6602338f33287a9aa4f194741e2538d033
xjin <mrtop@live.cn>
2019-11-06 16:45:46 +0800

    设备信息

---
+++ deviceInfo/deviceInfo.go
@@ -0,0 +1,87 @@
+package deviceInfo
+
+import (
+	"fmt"
+	"github.com/go-flutter-desktop/go-flutter"
+	"github.com/go-flutter-desktop/go-flutter/plugin"
+	"net"
+	"strings"
+)
+
+const (
+	channelName   = "device_info"
+	getDeviceInfo = "getDeviceInfo"
+)
+
+type DeviceInfoPlugin struct{}
+
+var _ flutter.Plugin = &DeviceInfoPlugin{}
+
+func (DeviceInfoPlugin) InitPlugin(messenger plugin.BinaryMessenger) error {
+	channel := plugin.NewMethodChannel(messenger, channelName, plugin.StandardMethodCodec{})
+	channel.HandleFunc(getDeviceInfo, getDeviceInfoFunc)
+
+	//channel := plugin.BasicMessageChannel(messenger, "\\\\.\\pipe\\VPNMainWindow", plugin.StandardMessageCodec{}}
+	//channel.HandleFunc(func(_ interface{}) (interface{}, error) { return nil, nil })
+	return nil
+}
+
+func getDeviceInfoFunc(arguments interface{}) (reply interface{}, err error) {
+
+	var macAddrs = getMacAddrs()
+	// var hostName = getHostname()
+	var ips = getIPs()
+
+	return map[interface{}]interface{}{
+		"macAddrs": strings.Replace(strings.Trim(fmt.Sprint(macAddrs), "[]"), " ", ",", -1),
+		"ips":      strings.Replace(strings.Trim(fmt.Sprint(ips), "[]"), " ", ",", -1),
+	}, nil
+
+	// return strings.Replace(strings.Trim(fmt.Sprint(list), "[]"), " ", ",", -1), nil
+}
+
+func getMacAddrs() (macAddrs []string) {
+	netInterfaces, err := net.Interfaces()
+	if err != nil {
+		fmt.Printf("fail to get net interfaces: %v", err)
+		return macAddrs
+	}
+
+	for _, netInterface := range netInterfaces {
+		macAddr := netInterface.HardwareAddr.String()
+		if len(macAddr) == 0 {
+			continue
+		}
+
+		macAddrs = append(macAddrs, macAddr)
+	}
+	return macAddrs
+}
+
+func getIPs() (ips []string) {
+
+	interfaceAddr, err := net.InterfaceAddrs()
+	if err != nil {
+		fmt.Printf("fail to get net interface addrs: %v", err)
+		return ips
+	}
+
+	for _, address := range interfaceAddr {
+		ipNet, isValidIpNet := address.(*net.IPNet)
+		if isValidIpNet && !ipNet.IP.IsLoopback() {
+			if ipNet.IP.To4() != nil {
+				ips = append(ips, ipNet.IP.String())
+			}
+		}
+	}
+	return ips
+}
+
+// func jsonToMap(jsonString string) map[string]interface{} {
+// 	var dat map[string]interface{}
+
+// 	if err := json.Unmarshal([]byte(jsonString), &dat); err == nil {
+// 		return dat
+// 	}
+// 	return nil
+// }
---
+++ deviceInfo/go.mod
@@ -0,0 +1,5 @@
+module github.com/FTDC/go-package/deviceInfo
+
+go 1.13
+
+require github.com/go-flutter-desktop/go-flutter v0.31.0 // indirect
